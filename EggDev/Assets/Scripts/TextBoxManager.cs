234a22a2d2cd8d4a7132bddf9523406beac7a4ba
Michael Ngo <michaelngo1337@yahoo.com>
2017-06-07 04:14:05 -0700

    Text Setup
    Made user input for text box. Used text box placeholder sprite.

---
+++ EggDev/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endLine;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endLine == 0)
+        {
+            endLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currentLine == textLines.Length)
+        {
+            currentLine = endLine;
+        }
+
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return) && currentLine < textLines.Length)
+        {
+            currentLine++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace) && currentLine > 0)
+        {
+            currentLine--;
+        }
+    }
+}
