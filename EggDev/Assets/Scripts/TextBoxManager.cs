fcc1b372c81b563ce6b0871fa754f9bd8083a595
Michael Ngo <michaelngo1337@yahoo.com>
2017-06-08 00:58:14 -0700

    Animated Text
    Made text animate based on speed.

--- EggDev/Assets/Scripts/TextBoxManager.cs
+++ EggDev/Assets/Scripts/TextBoxManager.cs
@@ -14,6 +14,11 @@ public class TextBoxManager : MonoBehaviour
     public int currentLine;
     public int endLine;
 
+    public bool isActive;
+
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+    public float typeSpeed;
 
     // Use this for initialization
     void Start()
@@ -27,27 +32,109 @@ public class TextBoxManager : MonoBehaviour
         {
             endLine = textLines.Length - 1;
         }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (!isActive)
+        {
+            return;
+        }
+
         if (currentLine == textLines.Length)
         {
             currentLine = endLine;
         }
 
-        theText.text = textLines[currentLine];
+        /*
+        else
+        {
+            StartCoroutine(TextScroll(textLines[currentLine]));
+        }
+        */
+
+        //theText.text = textLines[currentLine];
 
         if (Input.GetKeyDown(KeyCode.Return) && currentLine < textLines.Length)
         {
-            currentLine++;
+            if (!isTyping)
+            {
+                currentLine++;
+
+                if (currentLine > endLine)
+                {
+                    DisableTextBox();
+                }
+                else
+                {
+                    StartCoroutine(TextScroll(textLines[currentLine]));
+                }
+            }
+            else if (isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
+
         }
 
+        /*
         if (Input.GetKeyDown(KeyCode.Backspace) && currentLine > 0)
         {
-            currentLine--;
+            if (!isTyping)
+            {
+                currentLine--;
+            }
+            else if (isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
         }
+        */
+    }
+
+    // controls a line of text scrolling on the text box
+    private IEnumerator TextScroll (string lineOfText)
+    {
+        int letter = 0;
+        theText.text = "";
+        isTyping = true;
+        cancelTyping = false;
+
+        while (isTyping && !cancelTyping && letter < lineOfText.Length - 1)
+        {
+            theText.text += lineOfText[letter];
+            letter++;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+        theText.text = lineOfText;
+        isTyping = false;
+        cancelTyping = false;
+    }
+
+    // will use when necessary
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+        StartCoroutine(TextScroll(textLines[currentLine]));
+    }
+
+    // will use when necessary
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
     }
 }
