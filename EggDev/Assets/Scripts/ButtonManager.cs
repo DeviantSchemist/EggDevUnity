d7c59838349b00c73afa7438af9cb5a3db90b240
Michael Ngo <michaelngo1337@yahoo.com>
2017-06-20 04:23:57 -0700

    Made button manager script
    Added script to control button choices. Still incomplete, however it
    gets each button and puts it into an array for future use. Also created
    multiple tags for the buttons in order to sort them.

---
+++ EggDev/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Button[] buttons = (Button[]) FindObjectsOfType(typeof(Button));
+
+        // sorts buttons by their tags
+        Array.Sort(buttons, delegate (Button button1, Button button2)
+        {
+            return button1.tag.CompareTo(button2.tag);
+        });
+
+
+        //DEBUGGING STATEMENTS BELOW
+        /*
+        foreach (Button myButton in buttons)
+        {
+            Debug.Log(myButton.GetComponentInChildren<Text>().text + "\n");
+        }
+        */
+
+        /*
+        foreach (Button myButton in buttons)
+        {
+            Debug.Log(myButton);
+        }
+        */
+        //END OF DEBUGGING STATEMENTS
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
